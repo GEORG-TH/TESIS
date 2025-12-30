530d198fab4b7293a70cf456cff3053e41191f2d
Victor Jose Sanchez Laureano <107087963+SVIC92@users.noreply.github.com>
2025-12-30 17:44:02 -0500

    Correción definitiva de envio de correos

--- backend-inventario/src/main/java/com/inventario/backend_inventario/Service/EmailService.java
+++ backend-inventario/src/main/java/com/inventario/backend_inventario/Service/EmailService.java
@@ -1,19 +1,26 @@
 package com.inventario.backend_inventario.Service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value; // Importante
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmailService {
+
     @Autowired
     private JavaMailSender mailSender;
 
+    private String senderEmail = "swciplusinventarios@gmail.com";
+
     public void sendPasswordResetEmail(String to, String resetLink) {
-        SimpleMailMessage message = new SimpleMailMessage(); 
-        message.setTo(to); 
-        message.setSubject("Restablecimiento de Contraseña - SWCI+"); 
+        SimpleMailMessage message = new SimpleMailMessage();
+        
+        message.setFrom(senderEmail); 
+
+        message.setTo(to);
+        message.setSubject("Restablecimiento de Contraseña - SWCI+");
         message.setText("Hola,\n\nHas solicitado restablecer tu contraseña."
             + " Haz clic en el siguiente enlace para continuar:\n\n" + resetLink
             + "\n\nSi no solicitaste esto, ignora este correo." +
